b1d500ebdebe29a0d95b0f0bf51b3e5f5995be38
Sergey <sasovotimohinsergey@mail.ru>
2022-02-13 12:58:10 +0300

    Add net core empty project with AirportController

---
+++ CTeleport/Distance.API/Controllers/AirportController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Distance.API.Controllers
+{
+    [Route("airports")]
+    public class AirportController: ControllerBase
+    {
+
+        [HttpGet("{from}/distance/{to}")]
+        public Task<int> GetDistance(string from, string to)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ CTeleport/Distance.API/Startup.cs
@@ -0,0 +1,31 @@
+﻿namespace Distance.API;
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseHttpsRedirection();
+            app.UseHsts();
+        }
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
+    }
+}
